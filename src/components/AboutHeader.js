689ad244ba952e52deed15f2a7cd7f7348a24a49
jelon_zhang <jangdelong@qq.com>
2019-09-17 12:20:40 +0800

    react样式管理方案

--- src/components/AboutHeader.js
+++ src/components/AboutHeader.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import t from 'prop-types';
+import classnames from 'classnames';
+import '@/assets/scss/common.scss';
+import styles from './AboutHeader.module.scss';
 
 const AboutHeader = (props) => (
-  <h3>{ props.text || '这是公共头部' }</h3>
+  <h3 className={classnames(['title', styles.title])}>
+    { props.text || '这是公共头部' }
+    <span className={styles.inner}>6666</span>
+  </h3>
 )
 AboutHeader.propTypes = {
   text: t.string
